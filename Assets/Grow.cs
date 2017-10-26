19328d19b78a4a1d60c0e5eb56e68460c5151d60
Jorge Veiga de Souza Junior <jorgeveiga0@gmail.com>
2017-10-26 07:40:52 -0400

    Initial commit

---
+++ Assets/Grow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour {
+
+	Vector3 newScale;
+	float factor = 0.1f;
+	public bool canGrow = false;
+
+	void Update(){
+		if (canGrow) {
+			if (transform.localScale.x < 0.2f) {
+				factor += 0.01f;
+				newScale = new Vector3 (factor, factor, factor);
+				transform.localScale = newScale;
+			}
+		} else {
+			if (transform.localScale.x > 0.1f) {
+				factor -= 0.02f;
+				newScale = new Vector3 (factor, factor, factor);
+				transform.localScale = newScale;
+			}
+		}
+		canGrow = false;
+	}
+
+	void SelectedGrow(){
+		canGrow = true;
+	}
+}
---
+++ Assets/_Scripts/CursorBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorBehaviour : MonoBehaviour {
+
+	public Camera cam;
+	RaycastHit hit;
+	public Image gaze;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 100)){
+//			Debug.Log(hit.collider.name);
+			if (hit.collider.gameObject.tag == "Point") {
+				Grow temp = hit.collider.gameObject.GetComponent<Grow>();
+				temp.canGrow = true;
+			}
+			if ((hit.collider.gameObject.tag == "Point") && (gaze.fillAmount <= 1)) {
+				gaze.fillAmount += 0.03f;
+			} else {
+				gaze.fillAmount -= 0.03f;
+			}
+			if (gaze.fillAmount > 0.99f) {
+				Vector3 newPos = hit.collider.transform.position;
+				Debug.Log (hit.collider.transform.position);
+				transform.position = new Vector3(newPos.x, 0, newPos.z);
+				transform.rotation = hit.collider.transform.rotation;
+			}
+		}
+	}
+
+
+
+}
